35166e8bff5cf2f804e0f5b40d0c25ae726f8de3
SungMinju <minju2928@naver.com>
2025-07-26 13:15:43 +0900

    레지스토리 코드 추가

--- src/main/java/chic_chic/spring/domain/repository/ConsultPostRepository.java
+++ src/main/java/chic_chic/spring/domain/repository/ConsultPostRepository.java
@@ -1,8 +1,22 @@
 package chic_chic.spring.domain.repository;
 
 import chic_chic.spring.domain.ConsultPost;
+import chic_chic.spring.domain.enums.PostType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ConsultPostRepository extends JpaRepository<ConsultPost, Long> {
 
+    Optional<ConsultPost> findByConsultPost_Id(Long consultPostId);
+
+    //postType 별 최신 1개 조회
+    Optional<ConsultPost> findFirstByPostTypeOrderByCreatedAtDesc(PostType postType);
+
+    List<ConsultPost> findTop2ByPostTypeOrderByCreatedAtDesc(PostType postType);
+
+    Page<ConsultPost> findAllByPostType(PostType postType, Pageable pageable);
 }
