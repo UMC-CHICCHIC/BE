80beacd083e8dd5893df81465ae5f389b80fe2ff
LeeDohoun <dohoon9945@gmail.com>
2025-08-07 14:36:28 +0900

    개인제품추천

--- src/main/java/chic_chic/spring/service/product/HomeProductService.java
+++ src/main/java/chic_chic/spring/service/product/HomeProductService.java
@@ -2,6 +2,7 @@ package chic_chic.spring.service.product;
 
 import chic_chic.spring.domain.Product;
 import chic_chic.spring.domain.repository.ProductRepository;
+import chic_chic.spring.domain.repository.RecommendProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,9 +13,22 @@ import java.util.List;
 public class HomeProductService {
 
     private final ProductRepository productRepository;
+    private final RecommendProductRepository recommendProductRepository;
 
     // 인기 상품 4개 조회 (메인 페이지용)
     public List<Product> getPopularProducts() {
         return productRepository.findTop4ByOrderByItemRatingDesc();
+    }
+
+    // 개인 맞춤 추천 향수 4개 조회 (메인 페이지용)
+    public List<Product> getRecommendProducts(String memberEmail) {
+        List<Long> perfumeIds = recommendProductRepository
+                .findTop4ByMemberEmailOrderByCreatedAtDesc(memberEmail).stream()
+                .map(r -> r.getProductId())
+                .distinct()
+                .limit(4)
+                .toList();
+
+        return productRepository.findAllById(perfumeIds);
     }
 }
--- src/main/java/chic_chic/spring/web/dto/TempRequest.java
+++ src/main/java/chic_chic/spring/web/dto/TempRequest.java
@@ -1,4 +1,10 @@
 package chic_chic.spring.web.dto;
 
+import chic_chic.spring.domain.Product;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
 public class TempRequest {
 }
+
