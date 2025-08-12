f22f869b4910d3def9aca0460dd86e19f21d7031
J_lisa&& <155425927+SungMinju@users.noreply.github.com>
2025-08-12 22:39:16 +0900

    Update SecurityConfig.java
    https 테스트

--- src/main/java/chic_chic/spring/config/SecurityConfig.java
+++ src/main/java/chic_chic/spring/config/SecurityConfig.java
@@ -38,7 +38,8 @@ public class SecurityConfig {
                                 "/swagger-ui.html",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
-                                "/swagger-resources/**"
+                                "/swagger-resources/**",
+                                "/actuator/health"
                         ).permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
