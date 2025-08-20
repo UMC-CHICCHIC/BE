e0f28aaca21bdcfc500b207e25133cd561f39d50
namkungkang <namkung0131@naver.com>
2025-08-20 20:58:39 +0900

    1

--- src/main/java/chic_chic/spring/config/SecurityConfig.java
+++ src/main/java/chic_chic/spring/config/SecurityConfig.java
@@ -98,6 +98,7 @@ public class SecurityConfig {
                                 .userService(customOAuth2UserService)
                         )
                         .successHandler(customOAuth2SuccessHandler)
+
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
